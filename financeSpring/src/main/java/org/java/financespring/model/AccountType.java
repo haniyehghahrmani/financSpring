9c1a3d15c79613d539407bde5568fb2ac420a918
Asus <haniyehghahrmani@gmail.com>
2025-04-28 22:28:37 +0330

    Add AccountType Class

---
+++ financeSpring/src/main/java/org/java/financespring/model/AccountType.java
@@ -0,0 +1,32 @@
+package org.java.financespring.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "AccountTypeEntity")
+@Table(name = "AccountTypeTbl")
+public class AccountType {
+
+    @Id
+    @SequenceGenerator(name = "accountTypeSeq", sequenceName = "accountType_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accountTypeSeq")
+    @Column(name = "accountType_id")
+    private Long id;
+
+    @Column(name = "accountType_name", length = 200, nullable = false)
+    @NotBlank(message = "Account type name should not be blank")
+    @Size(min = 3, max = 200, message = "Account type name must be between 3 and 200 characters")
+    private String name;
+}
