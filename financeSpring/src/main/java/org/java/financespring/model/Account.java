ea4bec27379f9aa01c8024296f17ba1ee601c6e5
Asus <haniyehghahrmani@gmail.com>
2025-04-28 22:27:48 +0330

    Add Account Class

--- financeSpring/src/main/java/org/java/financespring/model/Account.java
+++ financeSpring/src/main/java/org/java/financespring/model/Account.java
@@ -1,14 +1,108 @@
 package org.java.financespring.model;
 
+import com.github.mfathi91.time.PersianDate;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
-@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "AccountEntity")
+@Table(name = "AccountTbl")
 public class Account {
+
     @Id
+    @SequenceGenerator(name = "accountSeq", sequenceName = "account_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "accountSeq")
+    @Column(name = "account_id")
     private Long id;
+
+    @Column(name = "account_account_name", columnDefinition = "NVARCHAR2(200)")
+    @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,200}$", message = "Invalid Account Name")
+    @Size(min = 3, max = 200, message = "Account name must be between 3 and 200 characters")
+    @NotBlank(message = "Should Not Be Null")
+    private String accountName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "accountType_id", nullable = false)
+    @NotNull(message = "Account type should not be null")
+    private AccountType accountType;
+
+    @Column(name = "account_amount", precision = 18, scale = 2, nullable = false)
+    @NotNull(message = "Balance should not be null")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Balance must be greater than zero")
+    private BigDecimal Balance;
+
+    @Column(name = "account_currency", columnDefinition = "NVARCHAR2(200)")
+    @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,200}$", message = "Invalid Currency")
+    @Size(min = 3, max = 200, message = "Currency must be between 3 and 200 characters")
+    @NotBlank(message = "Should Not Be Null")
+    private String Currency;
+
+    @Column(name = "account_created_date", nullable = false, updatable = false)
+    private LocalDate CreatedDate;
+
+    @Column(name = "account_last_updated")
+    private LocalDate LastUpdated;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "accountStatus_id", nullable = false)
+    @NotNull(message = "Account status should not be null")
+    private AccountStatus Status;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE})
+    @JoinColumn(name = "user_id", nullable = false)
+    private User ownerAccount;
+
+    @Column(name = "account_description", columnDefinition = "NVARCHAR2(200)")
+    @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,200}$", message = "Invalid Description")
+    @Size(min = 3, max = 200, message = "Description must be between 3 and 200 characters")
+//    @NotBlank(message = "Should Not Be Null")
+    private String description;
+
+    @Column(name = "account_opening_date", nullable = false)
+    @PastOrPresent(message = "Invalid Opening Date")
+    @NotNull(message = "Opening date should not be null")
+    private LocalDate openingDate;
+
+    @Transient
+    private String faOpeningDate;
+
+    @Column(name = "account_interest_rate", precision = 18, scale = 2, nullable = false)
+    @NotNull(message = "Interest rate should not be null")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Interest rate must be greater than zero")
+    private BigDecimal InterestRate;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "account")
     private List<Transaction> transactions;
+
+    @PrePersist
+    protected void onCreate() {
+        CreatedDate = LocalDate.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        LastUpdated = LocalDate.now();
+    }
+
+    public String getFaOpeningDate() {
+        return String.valueOf(PersianDate.fromGregorian(LocalDate.from(openingDate)));
+    }
+
+    public void setFaOpeningDate(String faOpeningDate) {
+        this.openingDate = LocalDate.from(PersianDate.parse(faOpeningDate).toGregorian().atStartOfDay());
+    }
 }
