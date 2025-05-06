f9bbf0c6c9cc6c54b6345e1a8c5cf11e9c4ede71
Asus <haniyehghahrmani@gmail.com>
2025-05-07 01:26:50 +0330

    add PayrollStatus

---
+++ financeSpring/src/main/java/org/java/financespring/model/PayrollStatus.java
@@ -0,0 +1,32 @@
+package org.java.financespring.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "PayrollStatusEntity")
+@Table(name = "PayrollStatusTbl")
+public class PayrollStatus {
+
+    @Id
+    @SequenceGenerator(name = "payrollStatusSeq", sequenceName = "payrollStatus_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payrollStatusSeq")
+    @Column(name = "payrollStatus_id")
+    private Long id;
+
+    @Column(name = "payrollStatus_name", length = 200, nullable = false)
+    @NotBlank(message = "Status name should not be blank")
+    @Size(min = 3, max = 200, message = "Status name must be between 3 and 200 characters")
+    private String name;
+}
