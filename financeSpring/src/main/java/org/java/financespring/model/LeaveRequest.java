272c8d47820e1ef0d46ff71bee0e82147e791d20
Asus <haniyehghahrmani@gmail.com>
2025-05-07 01:53:56 +0330

    add LeaveRequest _ LeaveStatus _ LeaveType

---
+++ financeSpring/src/main/java/org/java/financespring/model/LeaveRequest.java
@@ -0,0 +1,94 @@
+package org.java.financespring.model;
+
+import com.github.mfathi91.time.PersianDate;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "LeaveRequestEntity")
+@Table(name = "LeaveRequestTbl")
+public class LeaveRequest {
+
+    @Id
+    @SequenceGenerator(name = "leaveRequestSeq", sequenceName = "leaveRequest_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "leaveRequestSeq")
+    @Column(name = "leaveRequest_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "employee_id", nullable = false)
+    @NotNull(message = "Employee must not be null")
+    private Employee employee;
+
+    @Column(name = "start_date", nullable = false)
+    @NotNull(message = "Start date must not be null")
+    @FutureOrPresent(message = "Start date cannot be in the past")
+    private LocalDate startDate;
+
+    @Transient
+    private String faStartDate;
+
+    @Column(name = "end_date", nullable = false)
+    @NotNull(message = "End date must not be null")
+    @FutureOrPresent(message = "End date cannot be in the past")
+    private LocalDate endDate;
+
+    @Transient
+    private String faEndDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @NotNull(message = "Leave type must not be null")
+    private LeaveType type; // نوع مرخصی
+
+    @Column(name = "reason", length = 1000)
+    @Size(max = 1000, message = "Reason must be at most 1000 characters")
+    private String reason;
+
+    //تأیید شده؟
+    @Column(name = "is_approved", nullable = false)
+    private boolean approved = false;
+
+    @Column(name = "request_date", nullable = false)
+    @NotNull(message = "Request date must not be null")
+    @PastOrPresent(message = "Request date cannot be in the future")
+    private LocalDate requestDate;
+
+    @Column(name = "approval_date")
+    @PastOrPresent(message = "Approval date cannot be in the future")
+    private LocalDate approvalDate;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @NotNull(message = "Status must not be null")
+    private LeaveStatus status;
+
+    public String getFaStartDate() {
+        return String.valueOf(PersianDate.fromGregorian(LocalDate.from(startDate)));
+    }
+
+    public void setFaStartDate(String faStartDate) {
+        this.startDate = LocalDate.from(PersianDate.parse(faStartDate).toGregorian().atStartOfDay());
+    }
+
+    public String getFaEndDate() {
+        return String.valueOf(PersianDate.fromGregorian(LocalDate.from(endDate)));
+    }
+
+    public void setFaEndDate(String faEndDate) {
+        this.endDate = LocalDate.from(PersianDate.parse(faEndDate).toGregorian().atStartOfDay());
+    }
+}
---
+++ financeSpring/src/main/java/org/java/financespring/model/LeaveStatus.java
@@ -0,0 +1,32 @@
+package org.java.financespring.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "LeaveStatusEntity")
+@Table(name = "LeaveStatusTbl")
+public class LeaveStatus {
+
+    @Id
+    @SequenceGenerator(name = "leaveStatusSeq", sequenceName = "leaveStatus_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "leaveStatusSeq")
+    @Column(name = "leaveStatus_id")
+    private Long id;
+
+    @Column(name = "leavetatus_name", length = 200, nullable = false)
+    @NotBlank(message = "Status name should not be blank")
+    @Size(min = 3, max = 200, message = "Status name must be between 3 and 200 characters")
+    private String name;
+}
