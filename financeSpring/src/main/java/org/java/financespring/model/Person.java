845c54b9d848d63aeb4dbeb0705fd9173e6bbe7b
Asus <haniyehghahrmani@gmail.com>
2025-05-09 16:15:54 +0330

    updated

--- financeSpring/src/main/java/org/java/financespring/model/Person.java
+++ financeSpring/src/main/java/org/java/financespring/model/Person.java
@@ -1,6 +1,5 @@
 package org.java.financespring.model;
 
-import com.example.projectManagement.model.enums.Gender;
 import com.github.mfathi91.time.PersianDate;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
@@ -9,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.java.financespring.model.enums.Gender;
 
 import java.time.LocalDate;
 
@@ -19,34 +19,34 @@ import java.time.LocalDate;
 @SuperBuilder
 
 @Entity(name = "PersonEntity")
-@Table(name = "PersonTbl")
+@Table(name = "Persons")
 public class Person extends Base {
 
     @Id
     @SequenceGenerator(name = "personSeq", sequenceName = "person_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "personSeq")
-    @Column(name = "person_id")
+    @Column(name = "p_id")
     private Long id;
 
-    @Column(name = "person_name", columnDefinition = "NVARCHAR2(50)")
+    @Column(name = "p_name", columnDefinition = "NVARCHAR2(50)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Name")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     @NotBlank(message = "Should Not Be Null")
     private String name;
 
-    @Column(name = "person_lastname", columnDefinition = "NVARCHAR2(50)")
+    @Column(name = "p_lastname", columnDefinition = "NVARCHAR2(50)")
     @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Family")
     @Size(min = 3, max = 50, message = "Last Name must be between 3 and 50 characters")
     @NotBlank(message = "Should Not Be Null")
     private String lastname;
 
-    @Column(name = "person_national_id", length = 10, unique = true)
+    @Column(name = "p_national_id", length = 10, unique = true)
     @Pattern(regexp = "^[0-9]{1,10}$", message = "Invalid National ID")
     @Size(min = 1, max = 10, message = " National ID must be between 1 and 10 characters")
     @NotBlank(message = "Should Not Be Null")
     private String nationalId;
 
-    @Column(name = "person_birthdate")
+    @Column(name = "p_birthdate")
     @Past(message = "Invalid Birth Date")
     @NotNull(message = "Should Not Be Null")
     private LocalDate birthdate;
@@ -54,13 +54,10 @@ public class Person extends Base {
     @Transient
     private String faBirthdate;
 
-    @Column(name = "person_gender")
+    @Column(name = "p_gender")
     @Enumerated(EnumType.ORDINAL)
     @NotNull(message = "Should Not Be Null")
     private Gender gender;
-
-//    @OneToMany(cascade = {CascadeType.MERGE,CascadeType.PERSIST}, fetch = FetchType.LAZY,mappedBy = "person")
-//    private List<User> user;
 
     public String getFaBirthdate() {
         if (birthdate != null) {
@@ -74,5 +71,4 @@ public class Person extends Base {
             this.birthdate = PersianDate.parse(faBirthdate).toGregorian();
         }
     }
-
 }
---
+++ financeSpring/src/main/java/org/java/financespring/model/enums/Gender.java
@@ -0,0 +1,30 @@
+package org.java.financespring.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Gender {
+
+    MALE("Male", "مرد"),
+    FEMALE("Female", "زن");
+
+    private final String english;
+
+    private final String persian;
+
+    public String getTranslation(String language) {
+        return switch (language.toLowerCase()) {
+            case "english" -> getEnglish();
+            case "persian" -> getPersian();
+            default -> throw new IllegalArgumentException("Unsupported language: " + language);
+        };
+    }
+
+    @Override
+    public String toString() {
+        return english + " / " + persian;
+    }
+
+}
