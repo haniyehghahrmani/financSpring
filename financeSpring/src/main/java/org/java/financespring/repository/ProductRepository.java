e87f17c3f0bdc7ebc30135cd8847a15cb67caf21
Asus <haniyehghahrmani@gmail.com>
2025-05-09 16:18:21 +0330

    updated

---
+++ financeSpring/src/main/java/org/java/financespring/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.java.financespring.repository;
+
+import org.java.financespring.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
