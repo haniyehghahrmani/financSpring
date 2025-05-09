a3878974c3293ed12886dc1a27fe4230ad6cac2e
royazargar <roya.zargar1992@gmail.com>
2025-05-09 16:10:14 +0330

    updated

---
+++ financeSpring/src/main/java/org/java/financespring/service/impl/ProductService.java
@@ -0,0 +1,18 @@
+package org.java.financespring.service.impl;
+
+import org.java.financespring.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    Product save(Product product);
+
+    Product edit(Long id, Product product);
+
+    void remove(Long id);
+
+    List<Product> findAll();
+
+    Product findById(Long id);
+}
