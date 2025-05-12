e1dbcf6319ff5212715496893d275e5940ab9a25
Asus <haniyehghahrmani@gmail.com>
2025-05-12 20:16:11 +0330

    add controller

---
+++ financeSpring/src/main/java/org/java/financespring/controller/RoleController.java
@@ -0,0 +1,93 @@
+package org.java.financespring.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import org.java.financespring.exception.NoContentException;
+import org.java.financespring.model.Role;
+import org.java.financespring.service.RoleService;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/roles")
+public class RoleController {
+
+//    private final RoleService service;
+//
+//    public RoleController(RoleService service) {
+//        this.service = service;
+//    }
+//
+//    @GetMapping
+//    public String roleForm(Model model) {
+//        model.addAttribute("roleList", service.findAll());
+//        model.addAttribute("role", new Role());
+//        return "role";
+//    }
+//
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @ResponseBody
+//    public Role save(@Valid Role role, BindingResult result) {
+//        if (result.hasErrors()) {
+//            throw new ValidationException(
+//                    result.getAllErrors()
+//                            .stream()
+//                            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                            .toList().toString()
+//            );
+//        }
+//        return service.save(role);
+//    }
+//
+//    @PutMapping
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Role update(@Valid Role role, BindingResult result) throws NoContentException {
+//        if (result.hasErrors()) {
+//            throw new ValidationException(
+//                    result.getAllErrors()
+//                            .stream()
+//                            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                            .toList().toString()
+//            );
+//        }
+//        return service.edit(role);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @ResponseBody
+//    public void remove(@PathVariable Long id) throws NoContentException {
+//        service.logicalRemove(id);
+//    }
+//
+//    @GetMapping("/{id}")
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Optional<Role> findById(@PathVariable Long id) throws NoContentException {
+//        return service.findRoleByIdAndDeletedFalse(id);
+//    }
+//
+//    @GetMapping("/all")
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public List<Role> findAll() {
+//        return service.findRoleByDeletedFalse();
+//    }
+//
+//    @GetMapping("/findByRoleName")
+//    @ResponseStatus(HttpStatus.OK)
+//    @ResponseBody
+//    public Optional<Role> findByRoleName(@RequestParam String roleName) throws NoContentException {
+//        return service.findRoleByRoleNameAndDeletedFalse(roleName);
+//    }
+
+}
